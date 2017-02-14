99ed26836741058a28e096503b8fdacd39d41114
Daniel P Corson <danl@alum.mit.edu>
2017-02-13 16:05:08 -1000

    convert to sqlite so easier to use

--- src/Main.hs
+++ src/Main.hs
@@ -5,24 +5,36 @@ import Data.List
 import Data.Maybe
 import Data.Time.Clock.POSIX
 import Database.HDBC
-import Database.HDBC.PostgreSQL
+import Database.HDBC.Sqlite3
+import System.Directory
 import System.Environment
+import System.FilePath
 import qualified HSH
 
-cPS :: IO Connection
-cPS = handleSqlError $ connectPostgreSQL "dbname=me_log"
+getConnection :: IO Connection
+getConnection = do
+    home <- getHomeDirectory
+    let sqlFile = home </> ".config" </> "rnr"
+    dbExisted <- doesFileExist sqlFile
+    conn <- handleSqlError $ connectSqlite3 (home </> ".config" </> "rnr")
+    unless dbExisted . withTransaction conn $ \c -> run c (
+        "CREATE TABLE run_log (" ++
+        "command TEXT NOT NULL, " ++
+        "args TEXT NOT NULL, " ++
+        "did_time INTEGER, " ++
+        "PRIMARY KEY (command, args))") [] >> return ()
+    return conn
 
 getTimeInt :: IO Int
 getTimeInt = fmap floor getPOSIXTime
 
 escArgs = unwords . map escArg
 
-recordRun :: String -> [String] -> IO ()
-recordRun command args = do
+recordRun :: Connection -> String -> [String] -> IO ()
+recordRun conn command args = do
   time <- getTimeInt
-  conn <- cPS
   let argsSql = toSql $ escArgs args
-  withTransaction conn $ \ c -> do
+  withTransaction conn $ \c -> do
     numRowsChanged <- run c
       "UPDATE run_log SET did_time = ? WHERE command = ? AND args = ?"
       [toSql time, toSql command, argsSql]
@@ -31,10 +43,9 @@ recordRun command args = do
       [toSql command, argsSql, toSql time] >> return ()
   disconnect conn
 
-lastRun :: String -> [String] -> IO (Maybe Int)
-lastRun command args = do
-  conn <- cPS
-  ret <- withTransaction conn $ \ conn -> quickQuery conn
+lastRun :: Connection -> String -> [String] -> IO (Maybe Int)
+lastRun conn command args = do
+  ret <- withTransaction conn $ \c -> quickQuery c
     "SELECT did_time FROM run_log WHERE command = ? AND args = ? \
     \ORDER BY did_time DESC LIMIT 1"
     [toSql command, toSql $ escArgs args]
@@ -60,13 +71,14 @@ parseTimeStr s = let
 
 main :: IO ()
 main = do
+  conn <- getConnection
   timeStr:command:args <- getArgs
   timeNow <- getTimeInt
-  timeLastRunMb <- lastRun command args
+  timeLastRunMb <- lastRun conn command args
   let
     run = do
       HSH.runIO (command, args)
-      recordRun command args
+      recordRun conn command args
   case timeLastRunMb of
     Nothing -> run
     Just timeLastRun ->
